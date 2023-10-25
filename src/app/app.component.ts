65e7d64b92f67fc3e2e6c2101e4aaeaa6513a878
sumantestbook <suman@testbook.com>
2023-10-25 14:59:40 +0530

    route tab fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {Links, Tabs} from "./model/model";
 import {linkedIn, facebook, github, twitter} from  "./links/links"
 import {hello, about, project, contact} from  "./links/tabs"
+import {NavigationEnd, Event, Router} from "@angular/router";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,14 +13,30 @@ export class AppComponent implements OnInit{
   title = 'portfolio';
   tabs:Tabs[] = [hello, about, project, contact];
   links: Links [] = [linkedIn,facebook,twitter,github];
-  selectedIndex = 0;
-  constructor() {
+  selectedIndex: number = -1;
+
+  constructor(private router: Router) {
   }
 
   ngOnInit() {
+    this.router.events.subscribe((event:Event) => {
+      if (event instanceof NavigationEnd) {
+        this.getRoute(event.url)
+      }
+    });
   }
 
-  switchTab(index:number){
-    this.selectedIndex = index;
+  getRoute(param:any) {
+    const routeMap:{[key:string]:number} = {
+      '/home': 0,
+      '/about': 1,
+      '/project': 2,
+      '/contact': 3
+    };
+    if (param in routeMap) {
+      this.selectedIndex = routeMap[param];
+    } else {
+      this.selectedIndex = routeMap['/home'];
+    }
   }
 }
