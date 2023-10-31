1b9a3bece9802fb03999dcdee0123bf0a7e1cf5e
sumantestbook <suman@testbook.com>
2023-10-31 20:41:00 +0530

    added about me

--- src/app/common/about-me/about-me.component.ts
+++ src/app/common/about-me/about-me.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss']
 })
-export class AboutMeComponent implements OnInit {
+export class AboutMeComponent implements OnInit, AfterViewInit {
 
+  @ViewChild('element') element: ElementRef;
+  height:string;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(){
+    let eleHeight = this.element.nativeElement.offsetHeight;
+    this.height = eleHeight;
+    console.log(eleHeight)
+  }
 }
